ae8efeac2075ae0eea195f52e3bb3b3acf0b7d7b
Aque1228556367 <1228556367@qq.com>
2014-09-15 00:09:06 +0800

    张晨曦：增加编译fig实例的makefile

--- myproj/chAllName.sh
+++
@@ -1,6 +0,0 @@
-set -o nounset                              # Treat unset variables as an error
-
- for i in $(ls fig*)
- do   
-     mv "$i" "$i.c"
- done    
--- myproj/setup.sh
+++ myproj/setup.sh
@@ -1,2 +1,27 @@
-mkdir debug bin fig
+set -o nounset                              # Treat unset variables as an error
+
+if [ ! -d "debug" ]; 
+then
+  mkdir debug
+fi
+
+if [ ! -d "bin" ]; 
+then
+  mkdir bin
+fi
+
+if [ ! -d "fig" ]; 
+then
+  mkdir fig
+fi
+
+
+for i in $(ls ../src/fig/fig*)
+do  
+	echo ${i}
+	filename=${i#*../src/fig/}
+    cp "$i" "./fig/$filename.c"
+	sed -i '1i\#include "myerr.h"' "./fig/$filename.c"
+ done   
+ 
 # cp ../src/fig/fig1.3 ./fig/fig1.3.c
