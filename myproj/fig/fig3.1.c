a2f6df0771407a8cd79053099942db616c25dd33
Aque1228556367 <1228556367@qq.com>
2014-09-23 08:13:06 +0800

    张晨曦： 程序清单  3-1 ~ 3-2 修改

--- myproj/fig/fig3.1.c
+++ myproj/fig/fig3.1.c
@@ -5,6 +5,10 @@
 
 int main(void)
 {
+	// lseek (int fields, off_t offset, int whence)
+	// 若 whence 是 SEEK_SET，则将该文件的偏移量设置为据文件开始处 offset 个字节
+	// 若 whence 是 SEEK_CUR，则将该文件的偏移量设置其当前值加 offset，offset 可为正或负
+	// 若 whence 是 SEEK_END，则将该文件的偏移量设置为文件长度加 offset，offset 可为正或负
 	if (lseek(STDIN_FILENO, 0, SEEK_CUR) == -1)
 		printf("cannot seek\n");
 	else
@@ -12,3 +16,35 @@ int main(void)
 
 	exit(0);
 }
+
+// 思考：
+// $ ./test </etc/motd
+// seek ok
+// $ cat < /etc/motd |./test
+// cannot seek
+// ./test /var/spool/cron/FIFO
+// cannot seek
+
+// 为什么会有这种输出结果？
+// 注：“<”表示将stdin重定向到某文件，即：从文件读入某文件
+// 等价于 cat < /etc/motd
+// cat /etc/motd  和  cat < /etc/motd  结果是一样的
+
+
+// 参考：
+// http://www.cnblogs.com/lovemo1314/archive/2011/07/13/2105472.html
+
+//command > filename 　　　　　把标准输出重定向到一个新文件中
+//command >> filename 　　　　　把标准输出重定向到一个文件中(追加)
+//command 1 > fielname 　　　　　把标准输出重定向到一个文件中
+//command > filename 2>&1 　　　把标准输出和标准错误一起重定向到一个文件中
+//command 2 > filename 　　　　把标准错误重定向到一个文件中
+//command 2 >> filename 　　　　把标准输出重定向到一个文件中(追加)
+//command >> filename 2>&1 　　把标准输出和标准错误一起重定向到一个文件中(追加)
+//command < filename >filename2 　　把command命令以filename文件作为标准输入，以filename2文件作为标准输出
+//command < filename 　　　把command命令以filename文件作为标准输入
+//command << delimiter 　　把从标准输入中读入，直至遇到delimiter分界符
+//command <&m 　　　把文件描述符m作为标准输入
+//command >&m 　　　把标准输出重定向到文件描述符m中
+//command <&- 　　　把关闭标准输入
+
--- myproj/fig/fig3.2.c
+++ myproj/fig/fig3.2.c
@@ -1,3 +1,6 @@
+// 3.6 lseek 函数
+// 程序清单 3-2 创建一个具有空洞的文件(P66)
+// 产生空洞文件的方法：使文件的偏移量大于文件的当前长度
 #include "myerr.h"
 #include "apue.h"
 #include <fcntl.h>
@@ -5,8 +8,7 @@
 char	buf1[] = "abcdefghij";
 char	buf2[] = "ABCDEFGHIJ";
 
-int
-main(void)
+int main(void)
 {
 	int		fd;
 
