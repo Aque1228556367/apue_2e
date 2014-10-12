7f20d9ddd544ac230132ba8ddf1613cc211e915d
Aque1228556367 <1228556367@qq.com>
2014-10-12 10:40:49 +0800

    张晨曦：程序清单 4-1 ～ 4-3 修改

--- myproj/fig/fig4.8.c
+++ myproj/fig/fig4.8.c
@@ -1,5 +1,5 @@
-// 4.7 access 函数
-// 程序清单 4-2 access 函数实例(P92)
+// 4.8 access 函数
+// 程序清单 4-2 access 函数实例
 #include "myerr.h"
 #include "apue.h"
 #include <fcntl.h>
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 		err_quit("usage: a.out <pathname>");
 	if (access(argv[1], R_OK) < 0)
-		err_ret("access error for %s\n", argv[1]);
+		err_ret("access error for %s", argv[1]);
 	else
 		printf("read access OK\n");
 	if (open(argv[1], O_RDONLY) < 0)
-		err_ret("open error for %s\n", argv[1]);
+		err_ret("open error for %s", argv[1]);
 	else
 		printf("open for reading OK\n");
 	exit(0);
--- myproj/fig/fig4.9.c
+++ myproj/fig/fig4.9.c
@@ -1,17 +1,21 @@
+// 4.8 umask 函数
+// 程序清单 4-3 umask 函数实例
 #include "myerr.h"
 #include "apue.h"
 #include <fcntl.h>
 
 #define RWRWRW (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
 
-int
-main(void)
+int main(void)
 {
 	umask(0);
 	if (creat("foo", RWRWRW) < 0)
 		err_sys("creat error for foo");
+
+	// 修改当前进程的 umask 并不会对外层调用这个进程的 shell 的 umask 产生任何影响
 	umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 	if (creat("bar", RWRWRW) < 0)
 		err_sys("creat error for bar");
+
 	exit(0);
 }
