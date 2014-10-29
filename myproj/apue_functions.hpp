37d5178c2287e382d68cf7e4ae21269d0c10d803
Aque1228556367 <1228556367@qq.com>
2014-10-30 01:13:04 +0800

    张晨曦：更新apue函数一览

--- myproj/apue_functions.hpp
+++ myproj/apue_functions.hpp
@@ -30,68 +30,68 @@ ssize_t read(int fields, void* buf, size_t bytes);
 #include <unistd.h>	// Only for Unix
 ssize_t write(int fields, const void* buf, size_t bytes);
 
-// 3.11(P73) Ô­×Ó¶ÁĞ´
+// 3.11(P73) åŸå­è¯»å†™
 #include <unistd.h>	// Only for Unix
 ssize_t pread(int fields, void* buf, size_t bytes, off_t offset);
 ssize_t pwrite(int fields, const void* buf, size_t bytes, off_t offset);
 
-// 3.12(P74) ¸´ÖÆÒ»¸öÏÖ´æµÄÎÄ¼şÃèÊö·û fields
+// 3.12(P74) å¤åˆ¶ä¸€ä¸ªç°å­˜çš„æ–‡ä»¶æè¿°ç¬¦ fields
 #include <unistd.h>	// Only for Unix
 int dup(int fields);
 int dup2(int fields, int fields2);
 
-// 3.13(P75) ½«»º´æÖĞµÄĞÅÏ¢Ğ´Èë´ÅÅÌ£¬±£Ö¤ÎÄ¼şºÍ¸ßËÙ»º´æÄÚÈİÒ»ÖÂ
+// 3.13(P75) å°†ç¼“å­˜ä¸­çš„ä¿¡æ¯å†™å…¥ç£ç›˜ï¼Œä¿è¯æ–‡ä»¶å’Œé«˜é€Ÿç¼“å­˜å†…å®¹ä¸€è‡´
 #include <unistd.h>	// Only for Unix
 int fsync(int fields);
 int fdatasync(int fields);
 void sync(void);
 
-// 3.14(P76) ¸Ä±äÒÑ´ò¿ªµÄÎÄ¼şĞÔÖÊ
+// 3.14(P76) æ”¹å˜å·²æ‰“å¼€çš„æ–‡ä»¶æ€§è´¨
 #include <fcntl.h>
-int fcntl(int fields, int cmd, .. , /* int arg */);
+int fcntl(int fields, int cmd /* , int args .. */);
 
-// 3.15(P81) ÆäËü¶ÔI/OÍ¨µÀ½øĞĞ¹ÜÀíµÄ·½·¨
+// 3.15(P81) å…¶å®ƒå¯¹I/Oé€šé“è¿›è¡Œç®¡ç†çš„æ–¹æ³•
 #include <unistd.h>		/* System V */
 #include <sys/ioctl.h>	/* BSD and Linux */
 #include <stropts.h>	/* XSI Streams */
 int ioctl(int fields, int request, ...);
 
-// 4.2(P85) ÎÄ¼şµÄ×´Ì¬ÏêÏ¸ĞÅÏ¢
+// 4.2(P85) æ–‡ä»¶çš„çŠ¶æ€è¯¦ç»†ä¿¡æ¯
 /* 
- * ×¢£ºrestrict£¬CÓïÑÔÖĞµÄÒ»ÖÖÀàĞÍÏŞ¶¨·û£¨Type Qualifiers£©£¬
- * ÓÃÓÚ¸æËß±àÒëÆ÷£¬¶ÔÏóÒÑ¾­±»Ö¸ÕëËùÒıÓÃ£¬
- * ²»ÄÜÍ¨¹ı³ı¸ÃÖ¸ÕëÍâËùÓĞÆäËûÖ±½Ó»ò¼ä½ÓµÄ·½Ê½ĞŞ¸Ä¸Ã¶ÔÏóµÄÄÚÈİ¡£
+ * æ³¨ï¼šrestrictï¼ŒCè¯­è¨€ä¸­çš„ä¸€ç§ç±»å‹é™å®šç¬¦ï¼ˆType Qualifiersï¼‰ï¼Œ
+ * ç”¨äºå‘Šè¯‰ç¼–è¯‘å™¨ï¼Œå¯¹è±¡å·²ç»è¢«æŒ‡é’ˆæ‰€å¼•ç”¨ï¼Œ
+ * ä¸èƒ½é€šè¿‡é™¤è¯¥æŒ‡é’ˆå¤–æ‰€æœ‰å…¶ä»–ç›´æ¥æˆ–é—´æ¥çš„æ–¹å¼ä¿®æ”¹è¯¥å¯¹è±¡çš„å†…å®¹ã€‚
  */
 #include <sys/stat.h>
-int stat(const char* restrict pathname, struct stat* restrict buf);
+int stat(const char* __restrict pathname, struct stat* __restrict buf);
 int fstat(int fields, struct stat* buf);
-int lstat(const char* restrict pathname, struct stat* restrict buf);
+int lstat(const char* __restrict pathname, struct stat* __restrict buf);
 
-// 4.7(P92) È¨ÏŞ¿ØÖÆ
+// 4.7(P92) æƒé™æ§åˆ¶
 #include <unistd.h>		// Only for Unix
 int access(const char* pathname, int mode);
 
-// 4.8(P93) ÉèÖÃÎÄ¼şÄ£Ê½´´½¨ÆÁ±Î×Ö
+// 4.8(P93) è®¾ç½®æ–‡ä»¶æ¨¡å¼åˆ›å»ºå±è”½å­—
 #include <sys/stat.h>
 mode_t umask(mode_t cmask);
 
-// 4.9(P95) ¸ü¸ÄÏÖÓĞÎÄ¼şµÄ·ÃÎÊÈ¨ÏŞ
+// 4.9(P95) æ›´æ”¹ç°æœ‰æ–‡ä»¶çš„è®¿é—®æƒé™
 #include <sys/stat.h>
 int chmod(const char* pathname, mode_t mode);
 int fchmod(int fields, mode_t mode);
 
-// 4.11(P98) ¸ü¸ÄÎÄ¼şµÄÓÃ»§IDºÍ×éID
+// 4.11(P98) æ›´æ”¹æ–‡ä»¶çš„ç”¨æˆ·IDå’Œç»„ID
 #include <unistd.h>		// Only for Unix
 int chown(const char *pathname, uid_t owner, gid_t group);
 int fchown(int fields, uid_t owner, gid_t group);
 int lchown(const char *pathname, uid_t owner, gid_t group);
 
-// 4.13(P100) ÎÄ¼ş½Ø¶Ì
+// 4.13(P100) æ–‡ä»¶æˆªçŸ­
 #include <unistd.h>		// Only for Unix
 int truncate(const char *pathname, off_t length);
 int ftruncate(int fields, off_t length);
 
-// 4.15(P103) ÎÄ¼şÁ¬½Ó(Ó²Á¬½Ó)
+// 4.15(P103) æ–‡ä»¶è¿æ¥(ç¡¬è¿æ¥)
 #include <unistd.h>		// Only for Unix
 int link(const char *existingpath, const char *newpath);
 int unlink(const char *pathname);
@@ -100,12 +100,12 @@ int unlink(const char *pathname);
 int remove(const char *pathname);
 int rename(const char *oldname, const char * newname);
 
-// 4.17(P108) ·ûºÅÁ´½Ó(ÈíÁ¬½Ó)
+// 4.17(P108) ç¬¦å·é“¾æ¥(è½¯è¿æ¥)
 #include <unistd.h>		// Only for Unix
 int symlink(const char *actualpath, const char *sympath);
-ssize_t readlink(const char *restrict pathname, char *restrict buf, size_t bufsize);
+ssize_t readlink(const char* __restrict pathname, char* __restrict buf, size_t bufsize);
 
-// 4.19(P110) ÎÄ¼şµÄĞŞ¸ÄºÍ·ÃÎÊÊ±¼ä
+// 4.19(P110) æ–‡ä»¶çš„ä¿®æ”¹å’Œè®¿é—®æ—¶é—´
 #include <utime.h>		// Only for Unix
 int utime(const char * pathname, const struct utimbuf* times);
 
@@ -115,14 +115,14 @@ struct utimbuf
 	time_t modtime;	/*modification time*/
 };
 
-// 4.20(P111) ´´½¨/É¾³ıÄ¿Â¼
+// 4.20(P111) åˆ›å»º/åˆ é™¤ç›®å½•
 #include <sys/stat.h>
 int mkdir(const char* pathname, mode_t mode);
 
 #include <unistd.h>		// Only for Unix
 int rmdir(const char* pathname);
 
-// 4.21(P112) ¶ÁÄ¿Â¼
+// 4.21(P112) è¯»ç›®å½•
 #include <dirent.h>		// Only for Unix
 DIR* opendir(const char* pathname);
 
@@ -139,10 +139,10 @@ struct dirent
 {
 	ino_t d_ino;									/* i-node number */
 	char d_name[NAME_MAX + 1];		/* null-terminated('\0') filename */
-	......
+	/* ...... */
 };
 
-// 4.22(P116) ¹¤×÷Ä¿Â¼
+// 4.22(P116) å·¥ä½œç›®å½•
 #include <unistd.h>		// Only for Unix
 int chdir(const char *pathname);
 int fchdir(int fields);
@@ -150,17 +150,17 @@ int fchdir(int fields);
 char* getcwd(char* buf, size_t size);
 
 
-// 5.2 Á÷ºÍ I/O ¶ÔÏó
-// (P123)ÉèÖÃÁ÷µÄ¶¨Ïò
+// 5.2 æµå’Œ I/O å¯¹è±¡
+// (P123)è®¾ç½®æµçš„å®šå‘
 #include <stdio.h>
 #include <wchar.h>
-// ÈôÊÇ¿í×Ö½Ú¶¨Ïò·µ»ØÖµ > 0£¬ÈôÊÇ¿í×Ö½Ú¶¨Ïò·µ»ØÖµ < 0£¬Î´¶¨Ïò·µ»Ø 0
-// Èô mode < 0 fwide ½«ÊÔÍ¼Ê¹Ö¸¶¨µÄÁ÷ÊÇ×Ö½Ú¶¨Ïò
-// Èô mode > 0 fwide ½«ÊÔÍ¼Ê¹Ö¸¶¨µÄÁ÷ÊÇ¿í¶¨Ïò
+// è‹¥æ˜¯å®½å­—èŠ‚å®šå‘è¿”å›å€¼ > 0ï¼Œè‹¥æ˜¯å®½å­—èŠ‚å®šå‘è¿”å›å€¼ < 0ï¼Œæœªå®šå‘è¿”å› 0
+// è‹¥ mode < 0 fwide å°†è¯•å›¾ä½¿æŒ‡å®šçš„æµæ˜¯å­—èŠ‚å®šå‘
+// è‹¥ mode > 0 fwide å°†è¯•å›¾ä½¿æŒ‡å®šçš„æµæ˜¯å®½å®šå‘
 int fwide(FILE *fp, int mode);
 
-// 5.4(P124) »º³å
+// 5.4(P124) ç¼“å†²
 #include <stdio.h>
-int setbuf(FILE* restrict fp, char* restrict buf);
-int setvbuf(FILE* restrict fp, char* restrict buf, int mode, size_t size);
+int setbuf(FILE* __restrict fp, char* __restrict buf);
+int setvbuf(FILE* __restrict fp, char* __restrict buf, int mode, size_t size);
 
