3f7d23abf89875525cd2e819fcc0c5b5d1687bd3
Aque1228556367 <1228556367@qq.com>
2014-12-22 00:24:52 +0800

    张晨曦：程序清单 10-1 ~ 程序清单 10-2 修改

--- myproj/fig/fig10.2.c
+++ myproj/fig/fig10.2.c
@@ -1,10 +1,11 @@
+// 10.3 signal 函数
+// 程序清单 10-1 捕捉 SIGUSR1 和 SIGUSR2 的简单程序(P255)
 #include "myerr.h"
 #include "apue.h"
 
 static void	sig_usr(int);	/* one handler for both signals */
 
-int
-main(void)
+int main(void)
 {
 	if (signal(SIGUSR1, sig_usr) == SIG_ERR)
 		err_sys("can't catch SIGUSR1");
@@ -14,8 +15,7 @@ main(void)
 		pause();
 }
 
-static void
-sig_usr(int signo)		/* argument is signal number */
+static void sig_usr(int signo)		/* argument is signal number */
 {
 	if (signo == SIGUSR1)
 		printf("received SIGUSR1\n");
@@ -24,3 +24,26 @@ sig_usr(int signo)		/* argument is signal number */
 	else
 		err_dump("received signal %d\n", signo);
 }
+
+// 本例当以后台方式运行：./a.out &
+// 然后用 gdb attach 上去，在 singal 处设断点观察行为
+// 然后执行 kill -USR1 [进程ID] 和 kill -USR2 [进程ID] 观察输出结果
+
+// 注：
+// 可用 gdb 的 handle 命令来捕捉信号：
+// handle [信号名(如本例中的 SIGUSR1, SIGUSR2)] [选项]
+// 选项有下面这些可选：
+// nostop
+// 当被调试的程序收到信号时，GDB不会停住程序的运行，但会打出消息告诉你收到这种信号。
+// stop
+// 当被调试的程序收到信号时，GDB会停住你的程序。
+// print
+// 当被调试的程序收到信号时，GDB会显示出一条信息。
+// noprint
+// 当被调试的程序收到信号时，GDB不会告诉你收到信号的信息。
+// pass
+// noignore
+// 当被调试的程序收到信号时，GDB不处理信号。这表示，GDB会把这个信号交给被调试程序会处理。
+// nopass
+// ignore
+// 当被调试的程序收到信号时，GDB不会让被调试程序来处理这个信号。
--- myproj/fig/fig10.5.c
+++ myproj/fig/fig10.5.c
@@ -1,9 +1,15 @@
+// 10.6 可重入函数
+// 程序清单 10-2 在信号处理程序中调用不可重入函数(P261)
 #include "myerr.h"
 #include "apue.h"
 #include <pwd.h>
 
-static void
-my_alarm(int signo)
+// alarm 函数设置计时器超时，产生 SIGALRM 信号
+// getpwnam : 获取用户口令文件项(6.4节, P149)
+
+// getpwnam 不是signal可重入函数，如果在signal中重复定义，则会导致不确定的行为
+// 在Linux中，本程序会直接终止运行
+static void my_alarm(int signo)
 {
 	struct passwd	*rootptr;
 
@@ -13,18 +19,17 @@ my_alarm(int signo)
 	alarm(1);
 }
 
-int
-main(void)
+int main(void)
 {
-	struct passwd	*ptr;
+	struct passwd	*ptr = NULL;
 
 	signal(SIGALRM, my_alarm);
 	alarm(1);
-	for ( ; ; ) {
+	for ( ; ; )
+	{
 		if ((ptr = getpwnam("sar")) == NULL)
 			err_sys("getpwnam error");
 		if (strcmp(ptr->pw_name, "sar") != 0)
-			printf("return value corrupted!, pw_name = %s\n",
-					ptr->pw_name);
+			printf("return value corrupted!, pw_name = %s\n", ptr->pw_name);
 	}
 }
