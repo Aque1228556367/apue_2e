5168a5bb10e7c4b49f9b172be5560f9e8a101de1
Aque1228556367 <1228556367@qq.com>
2015-04-14 08:52:30 +0800

    张晨曦：程序清单 15-8 ~ 程序清单 15-9 修改

--- myproj/fig/fig15.17.c
+++ myproj/fig/fig15.17.c
@@ -1,20 +1,26 @@
+// 15.4 协同进程
+// 程序清单 15-8 对两个数求和的简单过滤程序(P423)
+// 作为 程序清单 15-9（fig15.18.c）的过滤程序，命名为 add2
 #include "myerr.h"
 #include "apue.h"
 
-int
-main(void)
+int main(void)
 {
 	int		n, int1, int2;
 	char	line[MAXLINE];
 
-	while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0) {
+	while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0)
+	{
 		line[n] = 0;		/* null terminate */
-		if (sscanf(line, "%d%d", &int1, &int2) == 2) {
+		if (sscanf(line, "%d%d", &int1, &int2) == 2)
+		{
 			sprintf(line, "%d\n", int1 + int2);
 			n = strlen(line);
 			if (write(STDOUT_FILENO, line, n) != n)
 				err_sys("write error");
-		} else {
+		}
+		else
+		{
 			if (write(STDOUT_FILENO, "invalid args\n", 13) != 13)
 				err_sys("write error");
 		}
